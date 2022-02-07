b51d5c22b04048a395c79f1fb8e9314738c9dcfb
Dmitry <82148972+dmzur@users.noreply.github.com>
2022-02-07 04:15:07 +0300

    lesson2

--- lesson/js/test.js
+++ lesson/js/test.js
@@ -1,27 +1,69 @@
 "use strict";
 
-const products = [
-    { id: 1, title: 'Notebook', price: 2000},
-    { id: 2, title: 'Mouse', price: 20},
-    { id: 3, title: 'Keyboard', price: 200},
-    { id: 4, title: 'Gamepad', price: 50},
-];
 
-const renderProduct = (item, img="img/2.jpg") => 
-            `<div class="product-item data-id=${item.id}">
-                <img src="${img}" alt="1">
-                <div class="card_desc">
-                    <h3>${item.title}</h3>
-                    <p>${item.price}</p>
-                    <button class="by-btn">Добавить</button>
-                </div>
-            </div>`;
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+        console.log(this.sumGoods());
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
 
 
-const renderPage = list => {
-    
-    document.querySelector('.products').insertAdjacentHTML("beforeend", list.map((item) => renderProduct(item)).join(''));
-    
-};
 
-renderPage(products);
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+            //           block.innerHTML += item.render();
+        }
+    }
+    sumGoods() {
+        return this.goods.reduce((sum, { price }) => sum + price, 0);
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+// class Cart { 
+//     addItems(){
+
+//     }
+//     delItems(){
+
+//     }
+//     render(){
+
+//     }
+// }
+// class CatrtItem {
+//     render(){
+
+//     }
+// }
+
+let list = new ProductList();
